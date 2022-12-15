5f7a30ce6f045777ea4cb70f516e90a87290dc95
rigood <rigood@naver.com>
2022-12-15 17:11:18 +0900

    Feat: 그 외 페이지 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Product from "./pages/Product";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 function App() {
-  return <Home />;
+  return <Cart />;
 }
 
 export default App;
